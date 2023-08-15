53eeab98a8b911fd6d36817342edc532fb773315
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-15 09:44:49 -0300

    Exercícios da lista 04.1 arrumados

--- lista041/questao13.py
+++ lista041/questao13.py
@@ -10,25 +10,22 @@ num2 = int(input("Informe o valor 2: "))
 num3 = int(input("Informe o valor 3: "))
 
 if num1 < num2 and num1 < num3:
-    print(f"{num1} ", end="")
     if num2 < num3:
-        print(num2, num3)
+        print(num1, num2, num3)
     else:
-        print(num3, num2)
+        print(num1, num3, num2)
 
 if num2 < num1 and num2 < num3:
-    print(f"{num2} ", end="")
     if num1 < num3:
-        print(num1, num3)
+        print(num2, num1, num3)
     else:
-        print(num3, num1)
+        print(num2, num3, num1)
 
 if num3 < num1 and num3 < num2:
-    print(f"{num3} ", end="")
     if num1 < num2:
-        print(num1, num2)
+        print(num3, num1, num2)
     else:
-        print(num2, num1)
+        print(num3, num2, num1)
 
 # Jeito não encadeado
 
