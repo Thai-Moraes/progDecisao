be55b2cc9ff3845081432863552138e8952e92e8
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-15 10:41:45 -0300

    Modificações

--- lista041/questao11.py
+++ lista041/questao11.py
@@ -5,4 +5,7 @@ somente o algarismo das centenas
 
 num = int(input("Informe um número de 3 algarismos: "))
 
-print(f"O algarismo das centenas de {num} é: {int(num / 100)}")
+if num < 100 or num > 999:
+    print("Número inválido! Tente novamente!")
+else:
+    print(f"O algarismo das centenas de {num} é: {int(num / 100)}")
